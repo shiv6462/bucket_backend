25a785e2ddaa3a97dd2ddd96504602ca4dc68fff
shiv6462 <66970186+shiv6462@users.noreply.github.com>
2021-11-24 22:07:02 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,10 @@
 terraform {
   required_version = ">= 0.12"
+    backend "gcs" {
+bucket = "my_backend_bucket"
+prefix = "network-tfstate"
+credentials = "my-first-project-331921-6472d80132f0.json"
+}
 
 }
 provider "google" {
