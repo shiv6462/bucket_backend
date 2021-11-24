9f4b5f344ca1b9f4be5891385515622e418bf0fc
shiv6462 <66970186+shiv6462@users.noreply.github.com>
2021-11-24 21:46:09 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,10 @@
 terraform {
   required_version = ">= 0.12"
+  backend "gcs" {
+bucket = "terraremote-bucket85558"
+prefix = "network-tfstate"
+credentials = "calcium-post-282611-3d088c407491.json"
+}
 }
 provider "google" {
   credentials = file(var.gcp_auth_file)
